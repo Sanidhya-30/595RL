473f384f57e6ddd0cb7bfc90fd69353f009a0248
Rohan Kamatar <rkamatar@mitre.org>
2024-11-27 21:06:55 -0500

    changes to default arguments and stopping criteria

--- src/train.py
+++ src/train.py
@@ -6,9 +6,10 @@ from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
 from pettingzoo.mpe import simple_tag_v3
 import supersuit as ss
 from ray.rllib.algorithms.ppo import PPOConfig
-from ray.rllib.algorithms.sac import SACConfig
+from ray.rllib.algorithms.sac import SACConfig, SAC
 from ray.rllib.algorithms.dqn import DQNConfig
 from ray.rllib.algorithms.ddpg import DDPGConfig
+
 
 def env_creator(num_good, num_adversaries, num_obstacles, max_cycles):
     """Create and preprocess the environment."""
@@ -17,7 +18,7 @@ def env_creator(num_good, num_adversaries, num_obstacles, max_cycles):
         num_adversaries=num_adversaries,
         num_obstacles=num_obstacles,
         max_cycles=max_cycles,
-        continuous_actions=True,
+        # continuous_actions=True,
     )
     env = ss.pad_observations_v0(env)
     env = ss.pad_action_space_v0(env)
@@ -25,7 +26,7 @@ def env_creator(num_good, num_adversaries, num_obstacles, max_cycles):
     env = ss.dtype_v0(env, "float32")  # Ensure observations are float32
     return env
 
-def get_config(args, env_name):
+def get_config(args, env_name, env):
     """Retrieve the configuration for the selected model."""
     if args.model == "ppo":
         config = (
@@ -45,12 +46,17 @@ def get_config(args, env_name):
             .environment(env=env_name)
             .framework("torch")
             .rollouts(num_rollout_workers=args.num_env_runners)
+            .resources(num_cpus_per_worker=1, num_gpus_per_worker=1/16)
             .training(
-                actor_lr=args.actor_lr,
-                critic_lr=args.critic_lr,
+                lr = args.actor_lr,
                 tau=args.tau,
                 train_batch_size=args.batch_size,
                 gamma=args.gamma,
+            )
+            .multi_agent(
+                policies={agent: (None, env.observation_space(agent), env.action_space(agent), {})
+                        for agent in env.possible_agents},
+                policy_mapping_fn=lambda agent_id, *args, **kwargs: agent_id,
             )
         )
     elif args.model == "dqn":
@@ -71,12 +77,38 @@ def get_config(args, env_name):
             .environment(env=env_name)
             .framework("torch")
             .rollouts(num_rollout_workers=args.num_env_runners)
+            .resources(num_cpus_per_worker=1, num_gpus_per_worker=1/16)
             .training(
                 actor_lr=args.actor_lr,
                 critic_lr=args.critic_lr,
                 tau=args.tau,
                 gamma=args.gamma,
                 train_batch_size=args.batch_size,
+            )
+            .multi_agent(
+                policies={agent: (None, env.observation_space(agent), env.action_space(agent), {})
+                        for agent in env.possible_agents},
+                policy_mapping_fn=lambda agent_id, *args, **kwargs: agent_id,
+            )
+        )
+    elif args.model == "single_sac":
+        config = (
+            SACConfig()
+            .environment(env=env_name)
+            .framework("torch")
+            .rollouts(num_rollout_workers=args.num_env_runners)
+            .resources(num_cpus_per_worker=1, num_gpus_per_worker=1/8)
+            .training(
+                lr = args.actor_lr,
+                tau=args.tau,
+                train_batch_size=args.batch_size,
+                gamma=args.gamma,
+            )
+            .multi_agent(
+                policies={agent: (None, env.observation_space(agent), env.action_space(agent), {})
+                        for agent in env.possible_agents},
+                policy_mapping_fn=lambda agent_id, *args, **kwargs: "agent_0" if "agent" in agent_id else "adversary_0" if "adversary" in agent_id else None,
+                policies_to_train = ['adversary_0']
             )
         )
     else:
@@ -85,8 +117,11 @@ def get_config(args, env_name):
     return config
 
 def train(args):
-    ray.init(ignore_reinit_error=True)
-    
+    ray.init(ignore_reinit_error=True,
+    num_cpus=8)
+    num_gpus_available = ray.cluster_resources().get("GPU", 0)  
+    print("Number of GPUS available: ", num_gpus_available) 
+
     # Create and register the environment
     env_name = "simple_tag_v3"
     tune.register_env(
@@ -96,24 +131,28 @@ def train(args):
         )
     )
 
+    env = env_creator(args.num_good, args.num_adversaries, args.num_obstacles, args.max_cycles)
+
     # Get the model configuration
-    config = get_config(args, env_name)
+    config = get_config(args, env_name, env)
 
     # Set up stop criteria
     stop = {
         "training_iteration": args.training_iterations,
-        "timesteps_total": args.timesteps_total,
-        "episode_reward_mean": args.reward_threshold,
+        # "timesteps_total": args.timesteps_total,
+        # "episode_reward_mean": args.reward_threshold,
     }
 
     # Train the model
     print(f"Training {args.model.upper()} model...")
     # result_dir = os.path.join(args.results_dir, args.model)
-    result_dir = os.path.join("/home/lucifer/595RL/src/results", args.model)
+    result_dir = os.path.join("/home/rkamatar/train_trial/results", args.model)
+    if args.model == 'single_sac': args.model = 'sac'
     results = tune.run(
         args.model.upper(),
         config=config.to_dict(),
         stop=stop,
+        # callbacks=callbacks,
         checkpoint_freq=args.checkpoint_freq,
         checkpoint_at_end=True,
         local_dir=result_dir,
@@ -126,21 +165,22 @@ def train(args):
         best_checkpoint = results.get_best_checkpoint(best_trial, metric="episode_reward_mean", mode="max")
         checkpoint_path = os.path.join(args.checkpoints_dir, f"{args.model}_best_checkpoint")
         os.makedirs(args.checkpoints_dir, exist_ok=True)
-        os.rename(best_checkpoint, checkpoint_path)
+        best_checkpoint.to_directory(checkpoint_path)
+        # os.rename(best_checkpoint.to_directory(), checkpoint_path)
         print(f"Best checkpoint saved at: {checkpoint_path}")
     else:
         print("No valid trial found. Training may not have completed successfully.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train RL agents on the SimpleTag environment.")
-    parser.add_argument("--model", type=str, choices=["ppo", "maddpg", "dqn", "ddpg", "sac"], required=True, help="Model to train.")
+    parser.add_argument("--model", type=str, choices=["ppo", "maddpg", "dqn", "ddpg", "sac", "single_sac"], required=True, help="Model to train.")
     parser.add_argument("--num_good", type=int, default=2, help="Number of good agents.")
     parser.add_argument("--num_adversaries", type=int, default=4, help="Number of adversary agents.")
-    parser.add_argument("--num_obstacles", type=int, default=2, help="Number of obstacles.")
-    parser.add_argument("--max_cycles", type=int, default=25, help="Max cycles per episode.")
-    parser.add_argument("--num_env_runners", type=int, default=4, help="Number of rollout workers.")
-    parser.add_argument("--actor_lr", type=float, default=1e-4, help="Learning rate for the actor.")
-    parser.add_argument("--critic_lr", type=float, default=1e-3, help="Learning rate for the critic.")
+    parser.add_argument("--num_obstacles", type=int, default=0, help="Number of obstacles.")
+    parser.add_argument("--max_cycles", type=int, default=100, help="Max cycles per episode.")
+    parser.add_argument("--num_env_runners", type=int, default=7, help="Number of rollout workers.")
+    parser.add_argument("--actor_lr", type=float, default=.001, help="Learning rate for the actor.")
+    parser.add_argument("--critic_lr", type=float, default=.005, help="Learning rate for the critic.")
     parser.add_argument("--tau", type=float, default=0.01, help="Soft update parameter.")
     parser.add_argument("--gamma", type=float, default=0.95, help="Discount factor.")
     parser.add_argument("--batch_size", type=int, default=1024, help="Training batch size.")
@@ -150,6 +190,6 @@ if __name__ == "__main__":
     parser.add_argument("--checkpoint_freq", type=int, default=10, help="Frequency of checkpoint saving.")
     parser.add_argument("--results_dir", type=str, default="results", help="Directory to save training results.")
     parser.add_argument("--checkpoints_dir", type=str, default="checkpoints", help="Directory to save checkpoints.")
+    
     args = parser.parse_args()
-
     train(args)
